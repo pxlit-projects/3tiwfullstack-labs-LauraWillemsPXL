fe7e32fe15fe0886e25cc7fb65e3f72e1c14294b
LauraWillemsPXL <laura.willems@student.pxl.be>
2024-11-13 09:33:19 +0100

    Finished Lab 8

--- backend-java/SpringCloudPXL/employee-service/src/main/java/be/pxl/services/controller/EmployeeController.java
+++ backend-java/SpringCloudPXL/employee-service/src/main/java/be/pxl/services/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import be.pxl.services.domain.dto.EmployeeRequest;
 import be.pxl.services.domain.dto.EmployeeResponse;
 import be.pxl.services.services.IEmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,15 +17,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EmployeeController {
     private final IEmployeeService employeeService;
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void addEmployee(@RequestBody EmployeeRequest employeeRequest) {
+        logger.debug("Adding a new employee with details: {}", employeeRequest);
         employeeService.addEmployee(employeeRequest);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<EmployeeResponse> findEmployeeById(@PathVariable Long id) {
+        logger.debug("Finding employee with id {}", id);
         return new ResponseEntity<>(employeeService.findById(id), HttpStatus.OK);
     }
 
